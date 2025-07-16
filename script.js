e9879d399feee74ff1b0226a4848e59dad7c9520
Seif Sedky <Seif.alaa1231@gmail.com>
2025-07-17 02:46:01 +0300

    Add core functions

--- script.js
+++ script.js
@@ -6,39 +6,94 @@ and vice versa when change names is clicked, you could add anmation before displ
 
 /* when change name restart grid and make it unclickable again */
 
-const gameDimensions = 3;
-let gameMoves = [gameDimensions][gameDimensions];
 
-function checkWin(gameMoves) {
-    const size = gameMoves.length;
-    const allEqual = (arr) => arr.every((val) => val && val === arr[i]);
 
-    //check rows
-    for (let i = 0; i < size; i++) {
-        if (allEqual(gameMoves[i]))
-            return true;
+
+
+
+function GridManager() {
+    const gameDimensions = 3;
+    let grid = [gameDimensions][gameDimensions];
+
+    function playMove(i, j, c) {
+        grid[i][j] = c;
     }
 
-    //check columns
-    for (let i = 0; i < size; i++) {
-        const column = gameMoves.map((row) => row[i]);
-        if (allEqual(column))
-            return true;
+    function resetMoves() {
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                grid[i][j] = null;
+            }
+        }
     }
 
 
-    //check diagonal
-    const diag = gameMoves.map((row, i) => row[i]);
-    if (allEqual(diag))
-        return true;
+    function checkWin(grid) {
+        const size = grid.length;
+        const allEqual = (arr) => arr.every((val) => val && val === arr[i]);
+
+        //check rows
+        for (let i = 0; i < size; i++) {
+            if (allEqual(grid[i]))
+                return true;
+        }
+
+        //check columns
+        for (let i = 0; i < size; i++) {
+            const column = grid.map((row) => row[i]);
+            if (allEqual(column))
+                return true;
+        }
 
 
-    //check anti diagonal 
-    const antiDiag = gameMoves.map((row, i) => row[(size-i)-1]);
-    if (allEqual(antiDiag))
-        return true;
+        //check diagonal
+        const diag = grid.map((row, i) => row[i]);
+        if (allEqual(diag))
+            return true;
 
-    return false;
+
+        //check anti diagonal 
+        const antiDiag = grid.map((row, i) => row[(size - i) - 1]);
+        if (allEqual(antiDiag))
+            return true;
+
+        return false;
+
+    }
 
 }
 
+function DisplayManager() {
+    function playMove() {
+
+    }
+
+    function resetMoves() {
+
+    }
+
+    function showWin() {
+
+    }
+}
+
+function gameManager() {
+}
+
+function eventManager() {
+    let p1="Player I";
+    let p2="Player II";
+
+    const startGame = () => { };
+
+    const endGame = () => { 
+        
+    };
+
+    const resetGame = () => { 
+        endGame();
+        startGame();
+    };
+
+    const changeNames = () => { };
+}
