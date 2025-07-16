a6fbe62e35660aedc413fe763f07f4e1e628985a
Seif Sedky <Seif.alaa1231@gmail.com>
2025-07-17 00:41:22 +0300

    Adjust title colouring

---
+++ script.js
@@ -0,0 +1,7 @@
+
+/* when start game display = none added to form and removed from gameplay-buttons
+and vice versa when change names is clicked, you could add anmation before display:none; but effort   */
+
+/* when someone wins, make all his cells winner */
+
+/* when change name restart grid and make it unclickable again */
